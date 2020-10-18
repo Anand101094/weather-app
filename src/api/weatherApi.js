5525aec6cdc8cd4a4c225d77eaced2094a9f32a1
Anand Kumar <anandkumar2991@gmail.com>
2020-10-18 23:39:54 +0530

    changes

--- src/api/weatherApi.js
+++ src/api/weatherApi.js
@@ -1,7 +1,7 @@
 const weatherApi = {
   getWeatherData: (payload) => {
     let searchTerm = payload ? payload : 'Mexico'
-    return fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${searchTerm}&APPID=75f972b80e26f14fe6c920aa6a85ad57&cnt=40&units=imperial`)
+    return fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${searchTerm}&APPID=75f972b80e26f14fe6c920aa6a85ad57&cnt=40&units=imperial`)
   },
 
 };
